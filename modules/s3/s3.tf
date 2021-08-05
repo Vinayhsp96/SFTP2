6fcb1bbf3b2811bae25b9794eca2fd111fecfa3e
Vinayhsp96 <60919781+Vinayhsp96@users.noreply.github.com>
2021-08-05 18:40:12 +0530

    Update s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,13 +1,13 @@
 #Creating S3 Bucket with versioning enable
 provider "aws" {
-  region = "${var.region}"
+  #region = "${var.region}"
 }
 
 resource "aws_s3_bucket" "bucket" {
 
   bucket = "${var.bucket-name}"
 
-  #region = "${var.region}"
+  region = "${var.region}"
 
   acl    = "${var.acl}"
 
